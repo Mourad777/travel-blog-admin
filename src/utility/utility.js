c52962ed4f2a8d9b495d4f085a6f09939c565f61
Mourad <mourad777b@gmail.com>
2021-08-22 14:25:10 -0400

    compress images prior to upload

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -43,8 +43,8 @@ export const resizeImageFn = async (file) => {
 
 
 
-export const AppUrl = 'http://localhost:8000/';
-// export let AppUrl = 'https://stormy-forest-71570.herokuapp.com/';
+// export const AppUrl = 'http://localhost:8000/';
+export let AppUrl = 'https://stormy-forest-71570.herokuapp.com/';
 // if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
 //     // AppUrl = 'http://localhost:8000/';
 //     AppUrl = 'https://stormy-forest-71570.herokuapp.com/';
